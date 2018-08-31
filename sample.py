aa85661793ab687f8b69370eef30c15a0061b03d
nirmal1064 <nirmal1064@gmail.com>
2018-09-01 01:13:27 +0530

    First Commit

---
+++ sample.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mlt
+import seaborn as sns
+mlt.style.use('fivethirtyeight')
+#import plotly.offlibe as py
+#import plotly.graph_objs as go
+#import plotly.tools as tls
+
+from subprocess import check_output
+#print(check_output(["ls","matches.csv"]).decode("utf8"))
